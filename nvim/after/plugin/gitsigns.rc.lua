917c4f9547c74cd2a4652b371e48be02d54ef810
ISON <azzgo@users.noreply.github.com>
2022-05-14 11:27:30 +0800

    feat: add hunk navigate and preview keymap

--- nvim/after/plugin/gitsigns.rc.lua
+++ nvim/after/plugin/gitsigns.rc.lua
@@ -3,3 +3,7 @@ local gitsigns = require("gitsigns")
 gitsigns.setup({
 	current_line_blame = true,
 })
+
+vim.api.nvim_set_keymap("n", "]h", ":Gitsigns next_hunk<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "[h", ":Gitsigns prev_hunk<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ghp", ":Gitsigns preview_hunk<CR>", { noremap = true, silent = true })
