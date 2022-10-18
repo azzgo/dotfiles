41cc7a1e9b77ed4535f2a33da99dcc3edb9dac01
Ison <azzgo@users.noreply.github.com>
2022-10-18 10:53:04 +0800

    feat: use regex hi for ts can not as js in vue ts

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,80 +1,81 @@
 vim.cmd([[packadd packer.nvim]])
 
 require("packer").init({
-	auto_clean = false,
+  auto_clean = false,
 })
 
 return require("packer").startup(function()
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
 
-	--- quick move
-	use("justinmk/vim-sneak")
+  --- quick move
+  use("justinmk/vim-sneak")
 
-	-- matchup
-	use("andymass/vim-matchup")
+  -- matchup
+  use("andymass/vim-matchup")
 
-	--  origin vim plugin use 'tpope/vim-surround'
+  --  origin vim plugin use 'tpope/vim-surround'
   use("machakann/vim-sandwich")
-	-- git cmmand support
-	use("tpope/vim-fugitive")
+  -- git cmmand support
+  use("tpope/vim-fugitive")
 
-	-- theme
-	-- use("NLKNguyen/papercolor-theme")
-	-- use("sainnhe/gruvbox-material")
-	use({ "shaunsingh/nord.nvim" })
+  -- theme
+  -- use("NLKNguyen/papercolor-theme")
+  -- use("sainnhe/gruvbox-material")
+  use({ "shaunsingh/nord.nvim" })
 
-	-- ##########lua plugins start##############
+  -- ##########lua plugins start##############
 
-	use("numToStr/Comment.nvim")
-	use("kyazdani42/nvim-web-devicons")
+  use("numToStr/Comment.nvim")
+  use("kyazdani42/nvim-web-devicons")
 
-	-- neotree
-	use({
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-	})
+  -- neotree
+  use({
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+  })
 
-	-- statusline
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons" },
-	})
+  -- statusline
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons" },
+  })
 
-	--  lsp config
-	use({ "neoclide/coc.nvim", branch = "release" })
+  --  lsp config
+  use({ "neoclide/coc.nvim", branch = "release" })
 
-	-- cmp only for cmdline
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
+  -- cmp only for cmdline
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-cmdline")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
 
-	-- syntax
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  -- syntax
+  use({ "nvim-treesitter/nvim-treesitter" })
+  use({ 'posva/vim-vue' })
+  -- fuzzy
+  use({ "nvim-telescope/telescope.nvim" })
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
 
-	use({ "nvim-telescope/telescope.nvim" })
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+  -- git
+  use({
+    "lewis6991/gitsigns.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+  })
 
-	-- git
-	use({
-		"lewis6991/gitsigns.nvim",
-		requires = { "nvim-lua/plenary.nvim" },
-	})
+  -- ufo
+  use({
+    "kevinhwang91/nvim-ufo",
+    requires = "kevinhwang91/promise-async",
+  })
 
-	-- ufo
-	use({
-		"kevinhwang91/nvim-ufo",
-		requires = "kevinhwang91/promise-async",
-	})
+  --- tabby
+  use("nanozuki/tabby.nvim")
 
-	--- tabby
-	use("nanozuki/tabby.nvim")
-
-	-- ###### lua plugin end ##########
+  -- ###### lua plugin end ##########
 end)
