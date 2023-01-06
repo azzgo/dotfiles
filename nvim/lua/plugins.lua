08fb76293d833c95d79af8e02c4555f699bf01b1
ISON <azzgo@users.noreply.github.com>
2023-01-06 20:34:23 +0800

    fix

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -70,5 +70,4 @@ return require("packer").startup(function()
     vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
   end})
 
-  -- ###### lua plugin end ##########
-end)
+  -- ###### lua plugin end ########## end)
