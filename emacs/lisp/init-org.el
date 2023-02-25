ae5480f80f3d65dc0d41716e2bd58c1095a1fb1c
Ison <azzgo@users.noreply.github.com>
2023-02-25 22:58:44 +0800

    feat-emacs: add org-roam-ui

--- emacs/lisp/init-org.el
+++ emacs/lisp/init-org.el
@@ -57,6 +57,18 @@
   )
 )
 
+(use-package org-roam-ui
+    :after (org-roam)
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
 ;; default capture
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 (setq org-capture-templates
