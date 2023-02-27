d8e0dc58cf04341d53b44a667cbd8c5d654176cf
ISON <azzgo@users.noreply.github.com>
2023-02-27 13:36:18 +0800

    emacs: update configs

--- emacs/init.el
+++ emacs/init.el
@@ -31,7 +31,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme plantuml-mode org-download ob-clojure cider clojure-mode use-package typescript-mode todotxt org-roam org-modern meow markdown-mode gruvbox-theme doom-modeline company cnfonts))
+   '(org-roam-ui zenburn-theme plantuml-mode org-download ob-clojure cider clojure-mode use-package typescript-mode todotxt org-roam org-modern meow markdown-mode gruvbox-theme doom-modeline company cnfonts))
  '(warning-suppress-log-types '(((org-roam)) ((org-roam))))
  '(warning-suppress-types '(((org-roam)))))
 (custom-set-faces
