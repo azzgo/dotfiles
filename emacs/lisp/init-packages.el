fff536cd569e02f3bab933b9449d168ce611103a
ISON <azzgo@users.noreply.github.com>
2023-02-27 13:36:18 +0800

    emacs: update configs

--- emacs/lisp/init-packages.el
+++ emacs/lisp/init-packages.el
@@ -1,5 +1,7 @@
-(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+
 (package-initialize) ;; You might already have this line
 
 ;;防止反复调用 package-refresh-contents 会影响加载速度
