73462157f6a0bdc4d145c00e720c484c248eed38
Ison <azzgo@users.noreply.github.com>
2023-03-05 22:56:43 +0800

    feat: replace plugin manager with lazy

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,96 +1,103 @@
-vim.cmd([[packadd packer.nvim]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require("packer").init({
-  auto_clean = false,
-})
-
-return require("packer").startup(function()
-  -- Packer can manage itself
-  use("wbthomason/packer.nvim")
-
+return require("lazy").setup({
   --- quick move
-  use { "justinmk/vim-sneak" }
-
+  "justinmk/vim-sneak",
   -- matchup
-  use("andymass/vim-matchup")
+  "andymass/vim-matchup",
 
   --  origin vim plugin use 'tpope/vim-surround'
-  use("tpope/vim-surround")
+  "tpope/vim-surround",
   -- git cmmand support
-  use { "tpope/vim-fugitive", config = function()
+  { "tpope/vim-fugitive", config = function()
     vim.cmd[[
       exe 'source' (g:vim_config_path . '/after/plugin/fugitive.vim')
     ]]
-  end}
+  end},
   -- theme
   -- use("NLKNguyen/papercolor-theme")
   -- use("sainnhe/gruvbox-material")
-  use({ "shaunsingh/nord.nvim" })
+  { "shaunsingh/nord.nvim", lazy = true },
 
   -- ##########lua plugins start##############
 
-  use { "numToStr/Comment.nvim", config = [[require('users.comment')]] }
-  use("kyazdani42/nvim-web-devicons")
+  { "numToStr/Comment.nvim", config = function ()
+   require('users.comment') 
+  end },
+  "kyazdani42/nvim-web-devicons",
 
   -- statusline
-  use({
+  {
     "nvim-lualine/lualine.nvim",
-    requires = { "kyazdani42/nvim-web-devicons" },
-    config = [[ require('users.lualine') ]]
-  })
+    dependencies = { "kyazdani42/nvim-web-devicons" },
+    config = function() require('users.lualine') end
+  },
 
   -- ranger
-  use({ 'francoiscabrol/ranger.vim',
-    config = function()
+  { 'francoiscabrol/ranger.vim',
+    init = function ()
       vim.g.ranger_map_keys = 0
       vim.g.ranger_replace_netrw = 1
+    end,
+    config = function()
       vim.keymap.set("n", "<leader>nn", vim.cmd.RangerWorkingDirectory, { noremap = true, silent = true })
       vim.keymap.set("n", "<leader>nf", vim.cmd.Ranger, { noremap = true, silent = true })
     end,
-    requires = { 'azzgo/bclose.vim' }
-  })
+    dependencies = { 'azzgo/bclose.vim' }
+  },
 
   --  lsp config
-  use({ "neoclide/coc.nvim", branch = "release",
+  { "neoclide/coc.nvim", branch = "release",
     config = function()
       vim.cmd[[
         let g:coc_config_home=g:dot_config_path . '/coc'
         exe 'source' (g:dot_config_path . '/coc/coc.vim')
       ]]
     end
-  })
+  },
 
 
   -- syntax
-  use({ "nvim-treesitter/nvim-treesitter", config = [[ require('users.treesitter') ]] })
+  { "nvim-treesitter/nvim-treesitter", config = function() require('users.treesitter') end },
   -- fuzzy
-  use({ "nvim-telescope/telescope.nvim", config = [[ require('users.telescope') ]] })
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+  { "nvim-telescope/telescope.nvim", config = function() require('users.telescope') end },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 
   -- git
-  use({
+  {
     "lewis6991/gitsigns.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-    config = [[ require('users.gitsigns') ]]
-  })
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() require('users.gitsigns') end
+  },
 
   -- ufo
-  use({
+  {
     "kevinhwang91/nvim-ufo",
-    requires = "kevinhwang91/promise-async",
-    config = [[ require('users.ufo') ]]
-  })
+    dependencies = "kevinhwang91/promise-async",
+    config = function() require('users.ufo') end
+  },
 
   --- tabby
-  use { "nanozuki/tabby.nvim", config = [[ require('users.tabby') ]] }
+  { "nanozuki/tabby.nvim", config = function() require('users.tabby') end },
 
   -- undotree
-  use({ "mbbill/undotree", config = function()
+  { "mbbill/undotree", config = function()
     vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, {})
-  end })
+  end },
 
   -- todo highlight
-  use({ "folke/todo-comments.nvim", ft = { "typescript", "javascript", "typescriptreact", "javascriptreact" }, config = [[ require('users.todo') ]] })
+  { "folke/todo-comments.nvim", ft = { "typescript", "javascript", "typescriptreact", "javascriptreact" }, config = function() require('users.todo') end },
 
   -- ###### lua plugin end ##########
-end)
+})
