aa8bd3e5fe61277ad19922a248fbaffcb8ef59a2
Ison <azzgo@users.noreply.github.com>
2023-03-20 07:58:05 +0800

    feat: update emacs

--- emacs/lisp/init-org.el
+++ emacs/lisp/init-org.el
@@ -48,12 +48,14 @@
 (setq org-agenda-files
    '("bullet.org" "gtd.org"))
 
+
+;;===================== ORG ROAM Block ==============
+
 ;; org-roam
 (use-package org-roam
   :bind
   (
     ("C-c n i" . org-roam-node-insert)
-    ("C-c n f" . org-roam-node-find)
   )
   :config
   (progn
@@ -74,7 +76,37 @@
           org-roam-ui-update-on-save t
           org-roam-ui-open-on-start nil))
 
-;; default capture
+(use-package consult-org-roam
+   :ensure t
+   :after org-roam
+   :init
+   (require 'consult-org-roam)
+   ;; Activate the minor mode
+   (consult-org-roam-mode 1)
+   :custom
+   ;; Use `ripgrep' for searching with `consult-org-roam-search'
+   (consult-org-roam-grep-func #'consult-ripgrep)
+   ;; Configure a custom narrow key for `consult-buffer'
+   (consult-org-roam-buffer-narrow-key ?r)
+   ;; Display org-roam buffers right after non-org-roam buffers
+   ;; in consult-buffer (and not down at the bottom)
+   (consult-org-roam-buffer-after-buffers t)
+   :config
+   ;; Eventually suppress previewing for certain functions
+   (consult-customize
+    consult-org-roam-forward-links
+    :preview-key (kbd "M-."))
+   :bind
+   ;; Define some convenient keybindings as an addition
+   ("C-c n f" . consult-org-roam-file-find)
+   ("C-c n b" . consult-org-roam-backlinks)
+   ("C-c n l" . consult-org-roam-forward-links)
+   ("C-c n r" . consult-org-roam-search))
+
+;;;=================================================
+
+
+;; default org capture
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "gtd.org" "Tasks")
