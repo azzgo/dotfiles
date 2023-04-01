ad32f7b1ddf005b91946501e97cad1c7cbffbee1
Ison <azzgo@users.noreply.github.com>
2023-04-01 10:45:51 +0800

    feat: update eamcs config

--- emacs/init.el
+++ emacs/init.el
@@ -25,18 +25,3 @@
 (require 'init-meow)
 (require 'init-org)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-roam-ui zenburn-theme plantuml-mode org-download ob-clojure cider clojure-mode use-package typescript-mode todotxt org-roam org-modern meow markdown-mode gruvbox-theme doom-modeline company cnfonts))
- '(warning-suppress-log-types '(((org-roam)) ((org-roam))))
- '(warning-suppress-types '(((org-roam)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "文泉驿等宽微米黑" :foundry "WQYF" :slant normal :weight normal :height 181 :width normal)))))
