88f9fbf8b054ed53fb9ed412af7f3a6377004d22
Ison <azzgo@users.noreply.github.com>
2023-04-14 23:32:04 +0800

    feat: emacs add consult base

--- emacs/lisp/init-evil.el
+++ emacs/lisp/init-evil.el
@@ -1,6 +1,14 @@
 (use-package evil
   :config (progn 
     (evil-mode 1)
+    (evil-set-undo-system 'undo-redo)
+    (evil-set-leader nil (kbd "SPC"))
+    (evil-define-key 'normal 'global (kbd "<leader>w") 'save-buffer)
+    (evil-define-key 'normal 'global (kbd "<leader>b") 'consult-buffer)
+    (evil-define-key 'normal 'global (kbd "<leader>rf") 'org-roam-node-find)
+    (evil-define-key 'normal 'global (kbd "<leader>nf") 'dired)
+    (evil-define-key 'normal 'global (kbd "<leader>f") 'consult-find)
+    (evil-define-key 'normal 'global (kbd "<leader>/") 'consult-ripgrep)
   )
 )
 
