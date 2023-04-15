a44a9369c4e74f0264d3b6edd2c1766f6a858617
Ison <azzgo@users.noreply.github.com>
2023-04-15 16:41:54 +0800

    feat: adjust of evil config

--- emacs/lisp/init-evil.el
+++ emacs/lisp/init-evil.el
@@ -4,11 +4,16 @@
     (evil-set-undo-system 'undo-redo)
     (evil-set-leader nil (kbd "SPC"))
     (evil-define-key 'normal 'global (kbd "<leader>w") 'save-buffer)
-    (evil-define-key 'normal 'global (kbd "<leader>b") 'consult-buffer)
-    (evil-define-key 'normal 'global (kbd "<leader>rf") 'org-roam-node-find)
+    (evil-define-key 'normal 'global (kbd "<leader>b") 'switch-to-buffer)
+    (evil-define-key 'normal 'global (kbd "<leader>r") 'org-roam-node-find)
+    (evil-define-key 'normal 'global (kbd "<leader>t") 'org-todo-list)
     (evil-define-key 'normal 'global (kbd "<leader>nf") 'dired)
     (evil-define-key 'normal 'global (kbd "<leader>f") 'consult-find)
     (evil-define-key 'normal 'global (kbd "<leader>/") 'consult-ripgrep)
+    (with-eval-after-load 'evil-maps
+      (define-key evil-insert-state-map (kbd "C-a") 'move-beginning-of-line)
+      (define-key evil-insert-state-map (kbd "C-e") 'move-end-of-line)
+    )
   )
 )
 
