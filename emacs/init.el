10e2b148cd8205eb834c94d41aec093d378140da
Ison <azzgo@users.noreply.github.com>
2023-05-20 23:10:19 +0800

    feat: require use-package first

--- emacs/init.el
+++ emacs/init.el
@@ -5,16 +5,6 @@
 ;; -----------------------------------------------------------------
 (require 'init-packages)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;; load use-package
-(eval-when-compile
-  ;; Following line is not needed if use-package.el is in ~/.emacs.d
-  (require 'use-package))
-
-(setq use-package-always-ensure t)
 
 (require 'init-local)
 (require 'init-ui)
