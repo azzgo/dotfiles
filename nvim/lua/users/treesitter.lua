0a3628ff83638c73ef9e5ce320ff6649eda37dfa
ISON <azzgo@users.noreply.github.com>
2023-05-20 13:30:43 +0800

    feat: enable treesitter and add some snippets

--- nvim/lua/users/treesitter.lua
+++ nvim/lua/users/treesitter.lua
@@ -23,7 +23,7 @@ treesitter_config.setup({
     },
   },
 	highlight = {
-		enable = false,
+		enable = true,
     disable = { "vim", "lua", "c" },
 		additional_vim_regex_highlighting = false,
 	},
