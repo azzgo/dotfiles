cc31d16b247b27147166d1e1da41af5b63e82a29
Ison <azzgo@users.noreply.github.com>
2023-06-10 20:11:33 +0800

    feat: lualine display bufpath

--- nvim/lua/users/lualine.lua
+++ nvim/lua/users/lualine.lua
@@ -4,9 +4,17 @@ if not ok then
 	return
 end
 
+function BufPath()
+  local bufPath = vim.fn.expand('%f')
+  return string.len(bufPath) > 100 and vim.fn.pathshorten(bufPath) or bufPath
+end
+
 lualine.setup({
 	sections = {
-		lualine_b = {},
+    lualine_b = {},
+		lualine_c = {
+      BufPath
+    },
 	},
   options = { theme = "auto", section_separators = "", component_separators = "" }
 })
