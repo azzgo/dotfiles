53cf5dff41490a566c8ded6ea844bd604d544665
ISON <azzgo@users.noreply.github.com>
2023-06-17 11:50:59 +0800

    feat: add rime to emacs

---
+++ emacs/lisp/init-rime.el
@@ -0,0 +1,21 @@
+(set-locale-environment "zh_CN.UTF-8")
+(set-language-environment "UTF-8")
+
+(use-package posframe)
+(use-package rime
+  :custom
+  (rime-librime-root "~/.emacs.d/librime")
+  (default-input-method "rime")
+  :config
+  (setq rime-disable-predicates
+	'(meow-normal-mode-p 
+	  meow-motion-mode-p
+          meow-keypad-mode-p))
+  (setq rime-posframe-properties
+      (list :background-color "#333333"
+            :foreground-color "#dcdccc"
+            :internal-border-width 10))
+  (setq rime-show-candidate 'posframe)
+)
+
+(provide 'init-rime)
