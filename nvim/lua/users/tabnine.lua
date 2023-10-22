6ef2c40c01c080d9875b4fb3dcee4eb8e98d7f38
ISON <azzgo@users.noreply.github.com>
2023-10-22 10:11:43 +0800

    feat: update tabnine complition key binding

--- nvim/lua/users/tabnine.lua
+++ nvim/lua/users/tabnine.lua
@@ -6,7 +6,7 @@ end
 
 tabnine.setup({
   disable_auto_comment=true,
-  accept_keymap="<Tab>",
+  accept_keymap="<C-Space>",
   dismiss_keymap = "<C-]>",
   debounce_ms = 800,
   suggestion_color = {gui = "#808080", cterm = 244},
