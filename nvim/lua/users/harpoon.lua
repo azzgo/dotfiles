5639f0e42df1d443dcb618a95d069280966733b2
ISON <azzgo@users.noreply.github.com>
2024-03-05 21:27:51 +0800

    feat: harpoon list with fzf

--- nvim/lua/users/harpoon.lua
+++ nvim/lua/users/harpoon.lua
@@ -6,34 +6,23 @@ end
 
 harpoon:setup()
 
-local extensions = require("harpoon.extensions")
-
-harpoon:extend(extensions.builtins.navigate_with_number())
-harpoon:extend({
-  UI_CREATE = function(cx)
-    vim.keymap.set("n", "<C-v>", function()
-      harpoon.ui:select_menu_item({ vsplit = true })
-    end, { buffer = cx.bufnr })
-
-    vim.keymap.set("n", "<C-x>", function()
-      harpoon.ui:select_menu_item({ split = true })
-    end, { buffer = cx.bufnr })
-
-    vim.keymap.set("n", "<C-t>", function()
-      harpoon.ui:select_menu_item({ tabedit = true })
-    end, { buffer = cx.bufnr })
-  end,
-  ADD = function(cx)
-    print("[harpoon] file added, location of", cx.item.value)
-  end,
-  REMOVE = function(cx)
-    print("[harpoon] file removed, location of", cx.item.value)
+local function harpoon_fzf_popup(harpoon_files)
+  local source = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(source, item.value)
   end
-})
+
+  vim.fn['fzf#run']({
+    sink = 'e',
+    -- options = "  --expect=ctrl-v,ctrl-x,ctrl-t",
+    window = { height = 0.6, width = 0.9 },
+    source = source
+  })
+end
 
 vim.keymap.set("n", "<A-a>", function() harpoon:list():append() end)
 vim.keymap.set("n", "<A-r>", function() harpoon:list():remove() end)
-vim.keymap.set("n", "<leader><tab>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+-- vim.keymap.set("n", "<leader><tab>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader><tab>", function() harpoon_fzf_popup(harpoon:list()) end)
 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
