718857a68bd830a23a43cd32c69460cd5f59c79f
ISON <azzgo@users.noreply.github.com>
2024-03-06 14:03:17 +0800

    feat: add back hook

--- nvim/lua/users/harpoon.lua
+++ nvim/lua/users/harpoon.lua
@@ -15,6 +15,15 @@ local function harpoon_fzf_popup(harpoon_files)
   vim.fn['_L_FZF_WRAPPER_RUN_']({ source = source })
 end
 
+harpoon:extend({
+  ADD = function(cx)
+    print("[harpoon] file added, location of", cx.item.value)
+  end,
+  REMOVE = function(cx)
+    print("[harpoon] file removed, location of", cx.item.value)
+  end
+})
+
 vim.keymap.set("n", "<A-a>", function() harpoon:list():append() end)
 vim.keymap.set("n", "<A-r>", function() harpoon:list():remove() end)
 -- vim.keymap.set("n", "<leader><tab>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
