1d56c76e87c5e9101c7f179a889b9ffac332f290
ISON <azzgo@users.noreply.github.com>
2024-03-16 00:29:57 +0800

    feat: debug false

--- nvim/lua/users/gen.lua
+++ nvim/lua/users/gen.lua
@@ -3,12 +3,13 @@ local ok, gen = pcall(require, "gen")
 if not ok then
   return
 end
+
 gen.setup({
   model = 'mistral',
   host = "localhost",         -- The host running the Ollama service.
   port = "11434",             -- The port on which the Ollama service is listening.
   display_mode = "float",     -- The display mode. Can be "float" or "split".
-  debug = true,
+  debug = false,
 })
 
 vim.keymap.set({ 'n', 'v' }, '<leader>]', ':Gen<CR>')
