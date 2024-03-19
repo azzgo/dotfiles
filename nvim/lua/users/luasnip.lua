028972c4c4e084278d2751217c60683bd4bfbf6a
ISON <azzgo@users.noreply.github.com>
2024-03-20 06:41:05 +0800

    feat: add some snippets

--- nvim/lua/users/luasnip.lua
+++ nvim/lua/users/luasnip.lua
@@ -9,46 +9,66 @@ local t = ls.text_node
 local i = ls.insert_node
 -- local sn = ls.snippet_node
 -- local isn = ls.indent_snippet_node
--- local f = ls.function_node
+local f = ls.function_node
 -- local c = ls.choice_node
 -- local d = ls.dynamic_node
 -- local r = ls.restore_node
 -- local events = require "luasnip.util.events"
 -- local ai = require "luasnip.nodes.absolute_indexer"
--- local extras = require "luasnip.extras"
--- local fmt = extras.fmt
--- local m = extras.m
+local fmt = require("luasnip.extras.fmt").fmt
 -- local l = extras.l
--- local postfix = require "luasnip.extras.postfix".postfix
+-- local m = extras.m
+local postfix = require "luasnip.extras.postfix".postfix
 
 -- keymap
-vim.keymap.set({"i"}, "<C-s>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-j>", function() ls.jump(1) end, {silent = true})
+vim.keymap.set({ "i" }, "<C-s>", function() ls.expand() end, { silent = true })
+vim.keymap.set({ "i", "s" }, "<C-j>", function() ls.jump(1) end, { silent = true })
 
 
 --- snippets
-local jsLogSnnipet = s("log", {
+local jsLogSnipet = s("log", {
   t("console.log("), i(1, "message"), t(");")
+})
+local jsLetPrefixSnipet = postfix('.let', {
+  f(function(_, parent)
+    return "let variable = " .. parent.snippet.env.POSTFIX_MATCH .. ";"
+  end, {}),
+})
+local jsConstPrefixSnipet = postfix('.const', {
+  f(function(_, parent)
+    return "let variable = " .. parent.snippet.env.POSTFIX_MATCH .. ";"
+  end, {}),
 })
 
 ls.add_snippets("typescript", {
-  jsLogSnnipet,
+  jsLogSnipet,
+  jsLetPrefixSnipet,
+  jsConstPrefixSnipet,
 })
 
 ls.add_snippets("javascript", {
-  jsLogSnnipet,
+  jsLogSnipet,
+  jsLetPrefixSnipet,
+  jsConstPrefixSnipet,
 })
 
 ls.add_snippets("javascriptreact", {
-  jsLogSnnipet,
+  jsLogSnipet,
+  jsLetPrefixSnipet,
+  jsConstPrefixSnipet,
 })
 
 ls.add_snippets("typescriptreact", {
-  jsLogSnnipet,
+  jsLogSnipet,
+  jsLetPrefixSnipet,
+  jsConstPrefixSnipet,
 })
 
+local formatString4GitMessage = [[{1}({2}): {3}
+
+{4}]]
 ls.add_snippets('gitcommit', {
-  s("msg", {
-    i(1, "feat"), t("("), i(2, "card_no"), t("): "), i(3, "messages")
-  })
+  s("msg", fmt(formatString4GitMessage, {
+      i(1, "feat"), i(2, "card_no"), i(3, "messages"), i(4, 'body')
+    }))
 })
