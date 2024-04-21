3da977c91b9fbb67365660c4ab8d6bee8548fb64
ISON <azzgo@users.noreply.github.com>
2024-04-21 21:47:28 +0800

    feat: change display relative path

--- nvim/lua/users/lualine.lua
+++ nvim/lua/users/lualine.lua
@@ -1,21 +1,21 @@
 local ok, lualine = pcall(require, "lualine")
 
 if not ok then
-	return
+  return
 end
 
 function BufPath()
   local bufPath = vim.fn.expand('%f')
-  return string.len(bufPath) > 100 and vim.fn.pathshorten(bufPath) or bufPath
+  local relativePath = vim.fn.fnamemodify(bufPath, ':.')
+  return string.len(relativePath) > 100 and vim.fn.pathshorten(relativePath) or relativePath
 end
 
 lualine.setup({
-	sections = {
+  sections = {
     lualine_b = { 'g:coc_status' },
-		lualine_c = {
-      BufPath
-    },
-    lualine_x = {'tabnine'}
-	},
-  options = { theme = "auto", section_separators = "", component_separators = "" }
+    lualine_c = { BufPath },
+    lualine_x = { 'tabnine' }
+  },
+  options = { theme = "auto", section_separators = "", component_separators = "" },
+  extensions = { 'oil' },
 })
