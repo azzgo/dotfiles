41b2f5ad3d91c0e09e7f0f8829bffd1b1cba4e2a
ISON <azzgo@users.noreply.github.com>
2024-05-11 13:52:02 +0800

    feat: add kimi and openrouter as backend

--- emacs/lisp/init-ai.el
+++ emacs/lisp/init-ai.el
@@ -11,4 +11,27 @@
   ))
 ))
 
+(gptel-make-openai
+  "kimi"
+  :host "api.moonshot.cn"
+  :stream t
+  :key #'gptel-api-key
+  :models '("moonshot-v1-8k" "moonshot-v1-32k" "moonshot-v1-128k")
+)
+
+
+(gptel-make-openai "OpenRouter"               ;Any name you want
+  :host "openrouter.ai"
+  :endpoint "/api/v1/chat/completions"
+  :stream t
+  :key #'gptel-api-key
+  :models '("openai/gpt-3.5-turbo"
+            "mistralai/mixtral-8x7b-instruct"
+            "meta-llama/codellama-34b-instruct"
+            "codellama/codellama-70b-instruct"
+            "google/palm-2-codechat-bison-32k"
+            "google/gemini-pro"))
+
+
+
 (provide 'init-ai)
