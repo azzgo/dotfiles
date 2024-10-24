39659049f1d61cdbf518044ca162b8356074d0c2
ISON <azzgo@users.noreply.github.com>
2024-10-24 19:47:17 +0800

    feat: show diffview view one commit

--- nvim/lua/users/diffview.lua
+++ nvim/lua/users/diffview.lua
@@ -1,5 +1,11 @@
 vim.keymap.set('n', '<leader>gl', vim.cmd.DiffviewFileHistory, {})
-vim.keymap.set('n', '<leader>gf', function ()
-  vim.cmd[[ DiffviewFileHistory % ]]
+vim.keymap.set('n', '<leader>gf', function()
+  vim.cmd [[ DiffviewFileHistory % ]]
 end, {})
 vim.keymap.set('n', '<leader>gd', ':DiffviewOpen', {})
+vim.keymap.set('n', '<leader>gc', function()
+  local commitId = vim.trim(vim.fn.input('Commit ID: '))
+  if commitId ~= '' then
+    vim.cmd('DiffviewOpen ' .. commitId .. '^!')
+  end
+end, {})
