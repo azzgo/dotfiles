be83b2fddf953e64270b96b48ed828fd839f1013
ISON <azzgo@users.noreply.github.com>
2024-11-02 12:06:26 +0800

    chore: remove org roam

--- emacs/lisp/init-org.el
+++ emacs/lisp/init-org.el
@@ -19,7 +19,7 @@
 ;; org-download
 (use-package org-download
   :after org
-  :config (progn
+  :init (progn
      (setq org-download-display-inline-images nil)
      (setq org-download-image-dir "./images")
      (setq org-download-image-org-width 600)
@@ -28,7 +28,7 @@
 
 (use-package d2-mode
   :after org
-  :config (setenv "PATH" (concat (getenv "PATH") ":" (getenv "HOME") "/.local/bin"))
+  :init (setenv "PATH" (concat (getenv "PATH") ":" (getenv "HOME") "/.local/bin"))
 )
 
 (use-package plantuml-mode
@@ -48,65 +48,6 @@
 ;; agenda files
 (setq org-agenda-files
    '("bullet.org" "gtd.org"))
-
-
-;;===================== ORG ROAM Block ==============
-
-;; org-roam
-(use-package org-roam
-  :bind
-  (
-    ("C-c r i" . org-roam-node-insert)
-    ("C-c r f" . org-roam-node-find)
-  )
-  :config
-  (progn
-    (setq org-roam-directory (file-truename (concat org-directory "/org-roam")))
-    (setq org-roam-db-location (file-truename (concat org-directory "/org-roam/org-roam.db")))
-    (org-roam-db-autosync-mode)
-    (require 'org-roam-export)
-  )
-)
-
-(use-package org-roam-ui
-    :after (org-roam)
-;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;;         a hookable mode anymore, you're advised to pick something yourself
-;;         if you don't care about startup time, use
-;;  :hook (after-init . org-roam-ui-mode)
-    :config
-    (setq org-roam-ui-sync-theme t
-          org-roam-ui-follow t
-          org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start nil))
-
-(use-package consult-org-roam
-   :ensure t
-   :after org-roam
-   :init
-   (require 'consult-org-roam)
-   ;; Activate the minor mode
-   (consult-org-roam-mode 1)
-   :custom
-   ;; Use `ripgrep' for searching with `consult-org-roam-search'
-   (consult-org-roam-grep-func #'consult-ripgrep)
-   ;; Configure a custom narrow key for `consult-buffer'
-   (consult-org-roam-buffer-narrow-key ?r)
-   ;; Display org-roam buffers right after non-org-roam buffers
-   ;; in consult-buffer (and not down at the bottom)
-   (consult-org-roam-buffer-after-buffers t)
-   :config
-   ;; Eventually suppress previewing for certain functions
-   (consult-customize
-    consult-org-roam-forward-links
-    :preview-key (kbd "M-."))
-   :bind
-   ;; Define some convenient keybindings as an addition
-   ("C-c n b" . consult-org-roam-backlinks)
-   ("C-c n l" . consult-org-roam-forward-links)
-   ("C-c n r" . consult-org-roam-search))
-
-;;;=================================================
 
 
 ;; default org capture
