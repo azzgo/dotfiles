be9d11b08bedc74a01d377e12ff47cf5e2577128
ISON <azzgo@users.noreply.github.com>
2025-02-22 15:00:37 +0800

    feat: add keyword to todo comments

--- nvim/lua/users/todo.lua
+++ nvim/lua/users/todo.lua
@@ -12,12 +12,23 @@ todo.setup {
       alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
       -- signs = false, -- configure signs for some keywords individually
     },
-    TODO = { icon = " ", color = "info" },
+    TODO = { icon = " ", color = "info" },
+    DOING = { icon = " ", color = "warning", alt = { "DOING", "WORKING" } },
+    DONE = { icon = " ", color = "success", alt = { "DONE", "COMPLETED" } },
     HACK = { icon = " ", color = "warning" },
     WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
     PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
     NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
     TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+  },
+  colors = {
+    error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+    warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+    info = { "DiagnosticInfo", "#2563EB" },
+    hint = { "DiagnosticHint", "#10B981" },
+    default = { "Identifier", "#7C3AED" },
+    test = { "Identifier", "#FF00FF" },
+    success = { "DiagnosticSuccess", "#22C55E" },
   },
   highlight = {
     before = "",
