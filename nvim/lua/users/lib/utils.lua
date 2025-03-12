5484d74b077170e26761cf48ff3af43a8a43f97e
ISON <azzgo@users.noreply.github.com>
2025-03-13 00:08:23 +0800

    feat: add todo snippet

--- nvim/lua/users/lib/utils.lua
+++ nvim/lua/users/lib/utils.lua
@@ -15,7 +15,7 @@ end
 ---@return table
 function M.merge_list(...)
   local result = {}
-  for _, t in ipairs({...}) do
+  for _, t in ipairs({ ... }) do
     for _, v in pairs(t) do
       table.insert(result, v)
     end
@@ -43,6 +43,26 @@ end
 
 function M.path_shorten(path, max_length)
   return string.len(path) > max_length and vim.fn.pathshorten(path) or path
+end
+
+local comment_ft_ok, comment_ft = pcall(require, 'Comment.ft')
+local comment_utils_ok, comment_utils = pcall(require, 'Comment.utils')
+
+--- Get the comment string {beg,end} table
+---@param ctype integer 1 for `line`-comment and 2 for `block`-comment
+---@return table comment_strings {begcstring, endcstring}
+function M.get_cstring(ctype)
+  if not comment_ft_ok or not comment_utils_ok then
+    return { '', '' }
+  end
+  local calculate_comment_string = comment_ft.calculate
+  local utils = comment_utils
+  -- use the `Comments.nvim` API to fetch the comment string for the region (eq. '--%s' or '--[[%s]]' for `lua`)
+  local cstring = calculate_comment_string { ctype = ctype, range = utils.get_region() } or vim.bo.commentstring
+  -- as we want only the strings themselves and not strings ready for using `format` we want to split the left and right side
+  local left, right = utils.unwrap_cstr(cstring)
+  -- create a `{left, right}` table for it
+  return { left, right }
 end
 
 return M
