25fd5b3964a6d8d67e177d0d3f8ca38d2e0a79c5
ISON <azzgo@users.noreply.github.com>
2025-03-12 23:10:54 +0800

    feat: remove if else to function map config

--- nvim/lua/users/self.lua
+++ nvim/lua/users/self.lua
@@ -7,7 +7,7 @@ local flash_ok, flash = pcall(require, 'flash')
 
 local last_run = nil
 
-local MENU_ENUM = {
+local MENU_LABEL_ENUM = {
   LAST_RUN = 'last run',
   SAVE_SESSION = 'save session',
   LOAD_SESSION = 'load session',
@@ -27,119 +27,148 @@ local MENU_ENUM = {
   FLASH_TREESITTER = 'flash treesitter',
   FLASH_JUMP_CWORD = 'flash jump cword',
   OPEN_QUICKFIX = 'open quickfix',
+  OPEN_LOCATION = 'open location',
   SNACKS_PICKER = 'snacks picker',
 }
 
+local MENU = {
+  [MENU_LABEL_ENUM.LOAD_SESSION] = function()
+    persistence.load({ last = true })
+  end,
+  [MENU_LABEL_ENUM.SELECT_SESSION] = function()
+    persistence.select()
+  end,
+  [MENU_LABEL_ENUM.SAVE_SESSION] = function()
+    persistence.save()
+    vim.notify('Session saved')
+  end,
+  [MENU_LABEL_ENUM.LIST_TODOS] = function()
+    Snacks.picker.todo_comments()
+  end,
+  [MENU_LABEL_ENUM.BUFFER_DELETE_OTHERS] = function()
+    helper.buffer_delete_others()
+    vim.notify('Other buffers deleted')
+  end,
+  [MENU_LABEL_ENUM.TOGGLE_COLORIZER] = function()
+    vim.fn['colorizer#ColorToggle']()
+    if vim.fn.exists('#Colorizer') == 1 then
+      Snacks.notify('Colorizer enabled', { title = 'Colorizer' })
+    else
+      Snacks.notify.warn('Colorizer disabled', { title = 'Colorizer' })
+    end
+  end,
+  [MENU_LABEL_ENUM.LUASNIP] = function()
+    helper.list_snippets()
+  end,
+  [MENU_LABEL_ENUM.COPY_BUFFER_RELATIVE_PATH] = function()
+    local bufPath = vim.fn.expand('%f')
+    local relativePath = vim.fn.fnamemodify(bufPath, ':.')
+    utils.copy_to_clipboard(relativePath)
+  end,
+  [MENU_LABEL_ENUM.COPY_BUFFER_FILE_NAME] = function()
+    local bufPath = vim.fn.expand('%f')
+    local fileName = vim.fn.fnamemodify(bufPath, ':t')
+    utils.copy_to_clipboard(fileName)
+  end,
+  [MENU_LABEL_ENUM.COPY_BUFFER_ABSOLUTE_PATH] = function()
+    local bufPath = vim.fn.expand('%f')
+    utils.copy_to_clipboard(bufPath)
+  end,
+  [MENU_LABEL_ENUM.KABAB_TO_CAMEL] = function()
+    local camelCase = utils.convertKababCaseToCamelCase(vim.fn.getreg('"'))
+    utils.copy_to_clipboard(camelCase)
+    Snacks.notify('Copied to clipboard: ' .. camelCase, { title = 'kabab to camel' })
+  end,
+  [MENU_LABEL_ENUM.CAMEL_TO_KABAB] = function()
+    local kababCase = utils.convertCamelCaseToKababCase(vim.fn.getreg('"'))
+    utils.copy_to_clipboard(kababCase)
+    Snacks.notify('Copied to clipboard: ' .. kababCase, { title = 'camel to kabab' })
+  end,
+  [MENU_LABEL_ENUM.PROJECTS] = function()
+    Snacks.picker.projects()
+  end,
+  [MENU_LABEL_ENUM.OPEN_LOCATION] = function()
+    Snacks.picker.locations()
+  end,
+  [MENU_LABEL_ENUM.SNACKS_PICKER] = function()
+    Snacks.picker()
+  end,
+  [MENU_LABEL_ENUM.FLASH_TREESITTER] = function()
+    flash.treesitter()
+  end,
+  [MENU_LABEL_ENUM.FLASH_JUMP_CWORD] = function()
+    flash.jump({
+      pattern = vim.fn.expand("<cword>"),
+    })
+  end,
+  [MENU_LABEL_ENUM.OPEN_QUICKFIX] = function()
+    vim.cmd.copen()
+  end,
+  [MENU_LABEL_ENUM.LIST_MARKS] = function()
+    Snacks.picker.marks()
+  end,
+  [MENU_LABEL_ENUM.EXPLORER] = function()
+    Snacks.explorer()
+  end,
+}
+
+
 local function self_use_case_popup()
   local menu = {}
-  table.insert(menu, MENU_ENUM.LAST_RUN)
+  table.insert(menu, MENU_LABEL_ENUM.LAST_RUN)
   if persistence_ok == true then
     vim.list_extend(menu, {
-      MENU_ENUM.SAVE_SESSION,
-      MENU_ENUM.LOAD_SESSION,
-      MENU_ENUM.SELECT_SESSION,
+      MENU_LABEL_ENUM.SAVE_SESSION,
+      MENU_LABEL_ENUM.LOAD_SESSION,
+      MENU_LABEL_ENUM.SELECT_SESSION,
     })
   end
   if luasnip_ok == true then
-    table.insert(menu, MENU_ENUM.LUASNIP)
+    table.insert(menu, MENU_LABEL_ENUM.LUASNIP)
   end
 
   if todo_ok == true then
-    table.insert(menu, MENU_ENUM.LIST_TODOS)
+    table.insert(menu, MENU_LABEL_ENUM.LIST_TODOS)
   end
   if flash_ok == true then
     vim.list_extend(menu, {
-      MENU_ENUM.FLASH_TREESITTER,
-      MENU_ENUM.FLASH_JUMP_CWORD,
+      MENU_LABEL_ENUM.FLASH_TREESITTER,
+      MENU_LABEL_ENUM.FLASH_JUMP_CWORD,
     })
   end
 
   if vim.g.loaded_colorizer == 1 then
-    table.insert(menu, MENU_ENUM.TOGGLE_COLORIZER)
+    table.insert(menu, MENU_LABEL_ENUM.TOGGLE_COLORIZER)
   end
 
   vim.list_extend(menu, {
-    MENU_ENUM.BUFFER_DELETE_OTHERS,
-    MENU_ENUM.COPY_BUFFER_RELATIVE_PATH,
-    MENU_ENUM.COPY_BUFFER_ABSOLUTE_PATH,
-    MENU_ENUM.COPY_BUFFER_FILE_NAME,
-    MENU_ENUM.KABAB_TO_CAMEL,
-    MENU_ENUM.CAMEL_TO_KABAB,
-    MENU_ENUM.LIST_MARKS,
-    MENU_ENUM.EXPLORER,
-    MENU_ENUM.LIST_ICONS,
-    MENU_ENUM.PROJECTS,
-    MENU_ENUM.OPEN_QUICKFIX,
-    MENU_ENUM.SNACKS_PICKER,
+    MENU_LABEL_ENUM.BUFFER_DELETE_OTHERS,
+    MENU_LABEL_ENUM.COPY_BUFFER_RELATIVE_PATH,
+    MENU_LABEL_ENUM.COPY_BUFFER_ABSOLUTE_PATH,
+    MENU_LABEL_ENUM.COPY_BUFFER_FILE_NAME,
+    MENU_LABEL_ENUM.KABAB_TO_CAMEL,
+    MENU_LABEL_ENUM.CAMEL_TO_KABAB,
+    MENU_LABEL_ENUM.LIST_MARKS,
+    MENU_LABEL_ENUM.EXPLORER,
+    MENU_LABEL_ENUM.PROJECTS,
+    MENU_LABEL_ENUM.OPEN_QUICKFIX,
+    MENU_LABEL_ENUM.OPEN_LOCATION,
+    MENU_LABEL_ENUM.SNACKS_PICKER,
   })
-
   vim.ui.select(menu, { prompt = 'quick actions: ' }, function(action)
-    if action == MENU_ENUM.LAST_RUN then
-      if last_run ~= nil then
-        action = last_run
-      else
-        Snacks.notify.warn('No last run found')
-        return
-      end
-    else
-      last_run = action
+    if action == nil then
+      return
     end
-    if action == MENU_ENUM.LOAD_SESSION then
-      persistence.load({ last = true })
-    elseif action == MENU_ENUM.SELECT_SESSION then
-      persistence.select()
-    elseif action == MENU_ENUM.SAVE_SESSION then
-      persistence.save()
-      vim.notify('Session saved')
-    elseif action == MENU_ENUM.BUFFER_DELETE_OTHERS then
-      helper.buffer_delete_others()
-      vim.notify('Other buffers deleted')
-    elseif action == MENU_ENUM.LUASNIP then
-      helper.list_snippets();
-    elseif action == MENU_ENUM.LIST_TODOS then
-      Snacks.picker.todo_comments()
-    elseif action == MENU_ENUM.COPY_BUFFER_RELATIVE_PATH then
-      local bufPath = vim.fn.expand('%f')
-      local relativePath = vim.fn.fnamemodify(bufPath, ':.')
-      utils.copy_to_clipboard(relativePath)
-    elseif action == MENU_ENUM.COPY_BUFFER_FILE_NAME then
-      local bufPath = vim.fn.expand('%f')
-      local fileName = vim.fn.fnamemodify(bufPath, ':t')
-      utils.copy_to_clipboard(fileName)
-    elseif action == MENU_ENUM.COPY_BUFFER_ABSOLUTE_PATH then
-      local bufPath = vim.fn.expand('%f')
-      utils.copy_to_clipboard(bufPath)
-    elseif action == MENU_ENUM.TOGGLE_COLORIZER then
-      vim.fn['colorizer#ColorToggle']()
-      if vim.fn.exists('#Colorizer') == 1 then
-        Snacks.notify('Colorizer enabled', { title = 'Colorizer' })
-      else
-        Snacks.notify.warn('Colorizer disabled', { title = 'Colorizer' })
-      end
-    elseif action == MENU_ENUM.KABAB_TO_CAMEL then
-      local camelCase = utils.convertKababCaseToCamelCase(vim.fn.getreg('"'))
-      utils.copy_to_clipboard(camelCase)
-      Snacks.notify('Copied to clipboard: ' .. camelCase, { title = 'kabab to camel' })
-    elseif action == MENU_ENUM.CAMEL_TO_KABAB then
-      local kababCase = utils.convertCamelCaseToKababCase(vim.fn.getreg('"'))
-      utils.copy_to_clipboard(kababCase)
-      Snacks.notify('Copied to clipboard: ' .. kababCase, { title = 'camel to kabab' })
-    elseif action == MENU_ENUM.LIST_MARKS then
-      Snacks.picker.marks()
-    elseif action == MENU_ENUM.EXPLORER then
-      Snacks.explorer()
-    elseif action == MENU_ENUM.PROJECTS then
-      Snacks.picker.projects()
-    elseif action == MENU_ENUM.FLASH_TREESITTER then
-      flash.treesitter()
-    elseif action == MENU_ENUM.FLASH_JUMP_CWORD then
-      flash.jump({
-        pattern = vim.fn.expand("<cword>"),
-      })
-    elseif action == MENU_ENUM.OPEN_QUICKFIX then
-      vim.cmd.copen()
-    elseif action == MENU_ENUM.SNACKS_PICKER then
-      Snacks.picker()
+    if action == MENU_LABEL_ENUM.LAST_RUN and last_run ~= nil then
+      action = last_run
+    else
+      Snacks.notify.warn('Last Action not found')
+      return
+    end
+
+    if MENU[action] then
+      MENU[action]()
+      last_run = action
     end
   end
   )
