fcef8418aaf42b9f661bc3246ac8051d1c681925
ISON <azzgo@users.noreply.github.com>
2025-03-23 16:27:43 +0800

    feat: update todo comment

--- nvim/lua/users/todo.lua
+++ nvim/lua/users/todo.lua
@@ -19,7 +19,8 @@ todo.setup {
     WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
     PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
     NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
-    TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+    TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED" } },
+    FAIL = { icn = '', color = "error", alt = { "FAILED" } },
   },
   colors = {
     error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
