42661b5f7da6ed51c465410c89b32a9c17d9557e
ISON <azzgo@users.noreply.github.com>
2026-04-16 21:09:35 +0800

    feat(float): hide all float window

--- nvim/lua/users/self.lua
+++ nvim/lua/users/self.lua
@@ -247,23 +247,16 @@ local function git_resolve_conflicts()
     end)
 end
 
-local function hide_last_float_in_current_tab()
+local function hide_all_floats_in_current_tab()
     local wins = vim.api.nvim_tabpage_list_wins(0)
-    local target_win = nil
-    local target_id = -1
 
     for _, win in ipairs(wins) do
         if vim.api.nvim_win_is_valid(win) then
             local config = vim.api.nvim_win_get_config(win)
-            if config.relative and config.relative ~= "" and win > target_id then
-                target_win = win
-                target_id = win
+            if config.relative and config.relative ~= "" then
+                vim.api.nvim_win_hide(win)
             end
         end
-    end
-
-    if target_win then
-        vim.api.nvim_win_hide(target_win)
     end
 end
 
@@ -274,7 +267,7 @@ vim.keymap.set({ "n", "v" }, "<A-g>", function()
         git_resolve_conflicts()
     end
 end)
-vim.keymap.set({ "n", "t", "v", "x" }, "<A-h>", hide_last_float_in_current_tab, { desc = "Hide last float in current tab" })
+vim.keymap.set({ "n", "t", "v", "x" }, "<A-h>", hide_all_floats_in_current_tab, { desc = "Hide all floats in current tab" })
 
 -- add font size increase and decrease to neovide
 if vim.g.neovide then
